bfe6578702eb19e7727fd78587d69b9241d46623
Ren <ren2410010182@gmail.com>
2026-06-20 08:02:59 +0800

    Selesai Bagian 2: Baca Tulis Berkas

--- src/bagian2/bacatulis/Latihan3.java
+++ src/bagian2/bacatulis/Latihan3.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class Latihan3 {
     public static void main(String[] args) {
-        // try-with-resources: stream otomatis ditutup setelah blok selesai
+        // try-with-resources: stream otomatis ditutup setelah blok selesai.
         try (PrintWriter penulis = new PrintWriter(new FileWriter("mahasiswa.txt"))) {
             penulis.println("Andi");
             penulis.println("Budi");
